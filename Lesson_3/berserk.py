ac4369d5d9a0a8ea8e767b26644f0ec3aacc4ad8
DarkValkier <darkvalkier@gmail.com>
2023-02-25 11:53:32 +0200

    Init

---
+++ Lesson_3/berserk.py
@@ -0,0 +1,21 @@
+from Lesson_2.character import Character
+
+
+class Berserk (Character):
+    def __init__(self, name='', health=100, damage=1, defence=0):
+        Character.__init__(self, name, health, damage, defence)
+
+        self.additional_damage = 0
+        self.max_health = health
+
+    def take_damage(self, damage):
+        super(Berserk, self).take_damage(damage=damage)
+
+    def attack(self, enemy):
+        self.additional_damage = \
+            max(
+                (1 - self.health / self.max_health) * self.damage,
+                0
+            )
+
+        enemy.take_damage(self.damage + self.additional_damage)
---
+++ Lesson_3/main.py
@@ -0,0 +1,19 @@
+from Lesson_2.character import Character
+from berserk import Berserk
+
+player1 = Character(name='Vasya', damage=10)
+player2 = Berserk(name='Petya', damage=10)
+
+print(f' - Player 1 - \n{player1}')
+print(f' - Player 2 - \n{player2}')
+
+while player1.is_alive() and player2.is_alive():
+    player1.attack(player2)
+    # player2.health -= max(player1.damage, 0)
+    print('Player 1 attacted Player 2')
+    player2.attack(player1)
+    print('Player 2 attacted Player 1')
+
+    print(f' - Player 1 - \n{player1}')
+    print(f' - Player 2 - \n{player2}')
+    print('\n\n')
